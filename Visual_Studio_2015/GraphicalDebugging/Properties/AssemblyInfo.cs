6b02ff8942ae0bfb263128ffc11d711c4452b52f
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2018-09-12 20:40:07 +0200

    Change name of the extension.

--- Visual_Studio_2015/GraphicalDebugging/Properties/AssemblyInfo.cs
+++ Visual_Studio_2015/GraphicalDebugging/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GraphicalDebugging")]
+[assembly: AssemblyTitle("Graphical Debugging")]
 [assembly: AssemblyDescription("Visual Studio 2015-2017 Graphical Debugging Tools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GraphicalDebugging")]
+[assembly: AssemblyProduct("Graphical Debugging")]
 [assembly: AssemblyCopyright("Adam Wulkiewicz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
