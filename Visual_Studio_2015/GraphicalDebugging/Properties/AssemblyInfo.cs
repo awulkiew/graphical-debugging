7d0a1bb9d2c73c0e5abdc8d3f219a746c9bd8873
Adam Wulkiewicz <adam.wulkiewicz@gmail.com>
2018-10-31 16:44:09 +0100

    Bump version (0.17), update release notes, improve description.

--- Visual_Studio_2015/GraphicalDebugging/Properties/AssemblyInfo.cs
+++ Visual_Studio_2015/GraphicalDebugging/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Graphical Debugging")]
-[assembly: AssemblyDescription("Visual Studio 2015-2017 Graphical Debugging Tools")]
+[assembly: AssemblyDescription("Visual Studio 2015, 2017 Graphical Debugging Tools")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Graphical Debugging")]
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.16.0.0")]
-[assembly: AssemblyFileVersion("0.16.0.0")]
+[assembly: AssemblyVersion("0.17.0.0")]
+[assembly: AssemblyFileVersion("0.17.0.0")]
